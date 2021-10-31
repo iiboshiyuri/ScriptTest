b8a5109fc610c58c1e540f8fc1b44d6635ac15ea
iiboshiyuri <92021544+iiboshiyuri@users.noreply.github.com>
2021-10-31 21:50:54 +0900

    lesson４の発展課題
    発展課題です。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,26 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+public class Boss
+{
+    private int mp = 53; //mp
+
+    //Magic関数
+    public void Magic()
+    {
+        
+        if (mp >= 5)
+        {
+            mp -= 5;
+            Debug.Log("魔法攻撃をした。残りMPは" + mp);
+        }
+        else
+        {
+            Debug.Log("MPが足りないため、魔法が使えない。");
+        }
+
+    }
+}
 public class Test : MonoBehaviour
 {
 
     // Use this for initialization
     void Start()
     {
-        //要素数が5に配列を初期化する
-        int[] array = new int[5];
-        //配列の各要素に値を代入する
-        array[0] = 10;
-        array[1] = 50;
-        array[2] = 30;
-        array[3] = 20;
-        array[4] = 40;
+        Boss lastboss = new Boss();
 
+        for (int i = 1; i <= 11; i++)
+        {
+            // Magic関数を呼び出す
+            lastboss.Magic();
+        }
+
+        int[] array = new int[5];
+
+        // 配列の各要素に値を代入する
+        array[0] = 30;
+        array[1] = 20;
+        array[2] = 50;
+        array[3] = 10;
+        array[4] = 80;
+
+        // 配列の要素をすべて表示する
         for (int i = 0; i < 5; i++)
         {
             Debug.Log(array[i]);
         }
-
         for (int i = 4; i > -1; i--)
         {
             Debug.Log(array[i]);
